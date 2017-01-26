ba1f6aec217a392365bb4fb65c1024d46eee4395
root <allberson85@gmail.com>
2017-01-25 21:06:52 -0300

    adding projects to monodevelop

---
+++ br.ufc.mdcc.hpc.shelf.tactical.environment.VerifyDataPort/src/1.0.0.0/BaseIVerifyServerPort.cs
@@ -0,0 +1,13 @@
+/* AUTOMATICALLY GENERATE CODE */
+
+using br.ufc.pargo.hpe.kinds;
+using br.ufc.mdcc.hpc.shelf.tactical.environment.VerifyDataPortServerType;
+using br.ufc.mdcc.hpc.storm.binding.environment.EnvironmentBindingBase;
+
+namespace br.ufc.mdcc.hpc.shelf.tactical.environment.VerifyDataPort
+{
+	public interface BaseIVerifyServerPort<S> : BaseIServerBase<S>, ISynchronizerKind 
+		where S:IVerifyDataPortServerType
+	{
+	}
+}
---
+++ br.ufc.mdcc.hpc.shelf.tactical.environment.VerifyDataPort/src/1.0.0.0/IVerifyServerPort.cs
@@ -0,0 +1,11 @@
+using br.ufc.pargo.hpe.kinds;
+using br.ufc.mdcc.hpc.shelf.tactical.environment.VerifyDataPortServerType;
+using br.ufc.mdcc.hpc.storm.binding.environment.EnvironmentBindingBase;
+
+namespace br.ufc.mdcc.hpc.shelf.tactical.environment.VerifyDataPort
+{
+	public interface IVerifyServerPort<S> : BaseIVerifyServerPort<S>, IServerBase<S>
+		where S:IVerifyDataPortServerType
+	{
+	}
+}
